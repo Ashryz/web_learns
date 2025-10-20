9360232c14cfb8d9deb2116958ec9ce25227fbc0
Ashryz <tarek.ashry9894@outlook.com>
2025-10-21 00:49:52 +0300

    [IMP] wb_poratal : adding list view

--- web_portal/controllers/portal.py
+++ web_portal/controllers/portal.py
@@ -7,13 +7,19 @@ class WbCustomerPortal(portal.CustomerPortal):
 
     def _prepare_home_portal_values(self, counters):
         values = super()._prepare_home_portal_values(counters)
-        patients = request.env['res.partner']
-        patients_count = patients.search_count([])
+        patients_count = len(
+            request.env['res.partner'].search([]).filtered(lambda p: any(c.name == 'patient' for c in p.category_id)))
         values['patients_count'] = patients_count
-        return  values
+        return values
 
     @http.route(['/my/patients', '/my/patients/page/<int:page>'], type='http', auth="user", website=True)
     def portal_my_patients(self, **kwargs):
-        values = self._prepare_sale_portal_rendering_values(quotation_page=False, **kwargs)
-        request.session['my_orders_history'] = values['orders'].ids[:100]
-        return request.render("sale.portal_my_orders", values)
+        values = self._prepare_portal_layout_values()
+        patients = request.env['res.partner'].search([]).filtered(
+            lambda p: any(c.name == 'patient' for c in p.category_id))
+
+        values.update({
+            'patients': patients,
+            'page_name': 'patients'
+        })
+        return request.render('web_portal.portal_my_patients_list',values)
